957335b894cbfc0569c6a26e93c8931679485d62
unknown <kadekyudaarianta@gmail.com>
2023-04-19 17:29:45 +0700

    membuat halam dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('content')
-  <h1 class="mt-4">Dashboard</h1>
+  <h1 class="mt-4">Haloo selamat datang di Dashboard</h1>
   <ol class="mb-4 breadcrumb">
     <li class="breadcrumb-item active">Dashboard</li>
   </ol>
